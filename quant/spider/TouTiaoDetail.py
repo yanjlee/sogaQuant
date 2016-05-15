74814a156b980eb69324c4499050b2e400178788
idoplay <hlxo@hotmail.com>
2016-05-16 06:57:26 +0800

    x

--- quant/spider/TouTiaoDetail.py
+++ quant/spider/TouTiaoDetail.py
@@ -24,12 +24,15 @@ class TouTiaoDetailSpider(SpiderEngine):
         self.tools.setup_logging(sys.argv[1], True, True)
         quncms_db = self.config['mysql']['quncms']
         mysql = sMysql(quncms_db['host'], quncms_db['user'], quncms_db['password'], quncms_db['dbname'])
-
+        i = 0
         while 1:
+            if i > 20:
+                break
             data = mysql.fetch_one("select * from video_contents where is_done=0")
             if data is None:
                 break
 
+            i += 1
             url = "http://www.toutiao.com%s" % data['item_seo_url']
             print url
             logging.debug('===%s===url:%s' % (data['itemid'], url))
